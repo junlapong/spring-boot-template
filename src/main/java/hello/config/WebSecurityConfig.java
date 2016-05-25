9efa86a54104abebb44d406287524210bac47b9c
Junlapong L <junlapong@gmail.com>
2016-05-25 22:35:03 +0700

    add security headers

---
+++ src/main/java/hello/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package hello.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity https) throws Exception {
+        https.authorizeRequests().antMatchers("/").permitAll().and()
+                .authorizeRequests().antMatchers("/console/**").permitAll();
+
+        //https.csrf().disable();
+        //https.headers().cacheControl().disable();
+        https.headers().frameOptions().sameOrigin();
+
+        //https.headers().addHeaderWriter(new StaticHeadersWriter("X-Custom-Header", "value"));
+    }
+
+}
