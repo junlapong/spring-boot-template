33bcd7e9e780fa7bee8deed4b9eb49fae4ae75e2
Junlapong L <junlapong@gmail.com>
2016-12-16 14:30:27 +0700

    update template

--- src/main/java/hello/controller/PaymentController.java
+++ src/main/java/hello/controller/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import hello.domain.PaymentForm;
+import java.util.Base64;
 
 @Controller
 public class PaymentController {
@@ -18,8 +19,18 @@ public class PaymentController {
     }
 
     @RequestMapping(value="/PaymentManager/PaymentInput.do", method=RequestMethod.POST)
-    public String paymentSubmit(@ModelAttribute PaymentForm greeting, Model model) {
-        model.addAttribute("greeting", greeting);
+    public String paymentSubmit(@ModelAttribute PaymentForm form, Model model) {
+
+        try {
+            byte[] bytes = Base64.getDecoder().decode(form.getContent());
+            String content = new String(bytes, "utf-8");
+            form.setContent(content);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        model.addAttribute("greeting", form);
         return "test-result";
     }
 
